bbb60a9ae79036c7b06d3d27220ced2b936729ae
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-04 22:20:06 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,8 @@ class Comment < ActiveRecord::Base
 
   def user_attributes=(user_attributes)
     if user_attributes[:username] != ""
-      self.build_user(user_attributes)
+      # self.build_user(user_attributes) # doesn't check if inputted username already exists
+      self.user = User.find_or_create_by(username: user_attributes[:username])
     end
   end
 
