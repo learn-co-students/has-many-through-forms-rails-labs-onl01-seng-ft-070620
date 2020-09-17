6f39c3e9fc49bffd817f29ddc153845c27ef11fc
cTraymany <chindalatht@gmail.com>
2020-09-17 18:03:26 +0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,31 @@
 class Post < ActiveRecord::Base
-  has_many :post_categories
   has_many :categories, through: :post_categories
+  has_many :post_categories
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :users
 
+
+  def categories_attributes=(attrs)
+    attrs.values.each do |attr|
+      # binding.pry
+      unless attr[:name].empty?
+        category = Category.find_or_create_by(attr)
+        self.categories << category
+      end
+    end
+  end
+
+  def users_attributes=(attrs)
+    attrs.values.each do |attr|
+      # binding.pry
+      unless attr[:username].empty?
+        user = User.find_or_create_by(attr)
+        self.users << user
+      end
+    end
+  end
+  
 end
